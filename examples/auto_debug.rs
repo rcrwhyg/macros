0d809fa3d29e37aaa3480145f5c882fc0849dcc0
rcrwhyg <rcrwhyg@sina.com>
2024-09-14 09:39:45 +0800

    feature: support parsing the attrs

--- examples/auto_debug.rs
+++ examples/auto_debug.rs
@@ -1,17 +1,19 @@
-use macros::AutoDeref;
+use macros::AutoDebug;
 
 #[allow(unused)]
-#[derive(Debug, AutoDeref)]
-#[deref(field = "inner")]
+#[derive(AutoDebug)]
 pub struct RespBulkString {
     inner: String,
+    #[debug(skip)]
     nothing: (),
+    hello: u32,
 }
 
 fn main() {
     let s = RespBulkString {
         inner: "hello".to_string(),
         nothing: (),
+        hello: 42,
     };
     println!("{:?}", s);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-mod auto;
+mod auto_debug;
+mod auto_deref;
 mod enum_from;
 mod enum_from_darling;
 
-// use auto::{process_auto_debug, process_auto_deref};
+use auto_debug::process_auto_debug;
+use auto_deref::process_auto_deref;
 use enum_from::process_enum_from;
 use enum_from_darling::process_enum_from_darling;
 use proc_macro::TokenStream;
@@ -24,18 +26,18 @@ pub fn derive_enum_from_darling(input: TokenStream) -> TokenStream {
     process_enum_from_darling(input).into()
 }
 
-// #[proc_macro_derive(AutoDeref, attributes(deref))]
-// pub fn derive_auto_deref(input: TokenStream) -> TokenStream {
-//     let input = syn::parse_macro_input!(input as syn::DeriveInput);
-//     // println!("{:#?}", input);
+#[proc_macro_derive(AutoDeref, attributes(deref))]
+pub fn derive_auto_deref(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    println!("{:#?}", input);
 
-//     process_auto_deref(input).into()
-// }
+    process_auto_deref(input).into()
+}
 
-// #[proc_macro_derive(AutoDebug, attributes(debug))]
-// pub fn derive_auto_debug(input: TokenStream) -> TokenStream {
-//     let input = syn::parse_macro_input!(input as syn::DeriveInput);
-//     // println!("{:#?}", input);
+#[proc_macro_derive(AutoDebug, attributes(debug))]
+pub fn derive_auto_debug(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    // println!("{:#?}", input);
 
-//     process_auto_debug(input).into()
-// }
+    process_auto_debug(input).into()
+}
